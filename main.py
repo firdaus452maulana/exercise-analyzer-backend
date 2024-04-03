6ddefa6223eb7474f459f77fe99d8ebe4371c3f4
firdaus452maulana <firdaus452maulana@gmail.com>
2024-04-03 18:39:04 +0900

    Add web syntax

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request
+from flask_restful import Api, Resource
+from json import loads
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
+api = Api(app)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == "POST":
+        file = request.files.get('file')
+        data = loads(file.read().decode('utf8'))
+        print(type(data))
+        return str(data[0])
+    return render_template('index.html')
