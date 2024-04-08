209c46c601c41842c404fbcfa1e522669f60b301
firdausmaulana <firdausmaulana@akupintar.id>
2024-04-08 11:09:45 +0900

    Add Feedback words

---
+++ dictionary_feedback.py
@@ -0,0 +1,11 @@
+# Get words about feedback
+def get_feedback_words():
+    words = {
+        -1: "",
+        0: "When performing squats, it is important to keep your back straight as well as bend forward slightly. Try to focus on placing your weight in your heels and keeping your chest lifted. This will help maintain balance and prevent unnecessary pressure on the lower back.",
+        1: "To improve your squat posture, it is important to keep your back straight and your chest lifted. Try to imagine that you are sitting on a low chair, with your weight on your heels and your knees in line with your feet. Also make sure your head and neck are in a neutral position.",
+        2: "In doing squats, keeping your thighs from dropping lower than your knees is key to avoiding injury. If your thighs drop too low, it could indicate that you need to strengthen your core and thigh muscles, as well as improve balance and flexibility.",
+        3: "It is important to keep the thighs parallel to the knees. Try to push the hips back and down until the thighs align with the knee position and form around 90-degree angle, while keeping the knees parallel to the feet.",
+    }
+
+    return words
