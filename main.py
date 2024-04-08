7eb0cddcf7aa53067604801f7875f5648abcaa06
firdausmaulana <firdausmaulana@akupintar.id>
2024-04-08 11:09:29 +0900

    Update front end

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ api = Api(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+def home():
     if request.method == "POST":
         file = request.files.get('file')
         data = loads(file.read())
+        data_update = {
+            "data": data,
+            "feedback_db": "test"
+        }
         # print(data[0])
         # print(type(data))
-        return render_template('show.html', data=data)
+        return render_template('show.html', data=data_update)
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
