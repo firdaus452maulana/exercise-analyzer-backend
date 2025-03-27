a14a8585a70898456936c4a39ecde70ab5d8bb08
firdaus452maulana <firdaus452maulana@gmail.com>
2025-03-27 22:29:00 +0900

    Fix Firebase formatting

--- firestore_service.py
+++ firestore_service.py
@@ -4,14 +4,14 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 load_dotenv()
-print(os.getenv("FIREBASE_TYPE"))
+
 def initialize_firebase():
     if not firebase_admin._apps:
         service_account_key = {
             "type": os.getenv("FIREBASE_TYPE"),
             "project_id": os.getenv("FIREBASE_PROJECT_ID"),
             "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-            "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace(r'\n', '\n'),
             "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
             "client_id": os.getenv("FIREBASE_CLIENT_ID"),
             "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
@@ -20,7 +20,6 @@ def initialize_firebase():
             "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
             "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
         }
-        print(service_account_key)
         cred = credentials.Certificate(service_account_key)
         firebase_admin.initialize_app(cred)
 
