53c3f688918f2d7d6e1f495a3884bc1f0d795c41
firdaus452maulana <firdaus452maulana@gmail.com>
2025-03-27 22:51:14 +0900

    debug

--- main.py
+++ main.py
@@ -14,20 +14,22 @@ api = Api(app)
 
 load_dotenv()
 
-service_account_key = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
-    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
-}
-print(service_account_key)
+# service_account_key = {
+#     "type": os.getenv("FIREBASE_TYPE"),
+#     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+#     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+#     "private_key": ,
+#     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+#     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+#     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+#     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+#     "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+#     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+#     "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+# }
+print(os.getenv("FIREBASE_PRIVATE_KEY").replace("\\\n", "\n"))
+print(os.getenv("FIREBASE_PRIVATE_KEY").replace("\n", "\n"))
+print(os.getenv("FIREBASE_PRIVATE_KEY"))
 
 @app.route('/analyze', methods=['GET', 'POST'])
 def home():
